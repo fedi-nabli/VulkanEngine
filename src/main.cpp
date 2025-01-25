c16cc193c4ace3378b5b2745a53e4517f7d4d713
fedi-nabli <fedinabli@gmail.com>
2025-01-25 16:22:03 +0100

    Initial commit: Project setup

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <cmath>
+#include <cstdint>
+
+#include <gsl/gsl>
+
+int main(std::int32_t argc, gsl::zstring* argv)
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
