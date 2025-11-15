d9c3b75dfb205616602edeccc2656fc417a608a5
fedi-nabli <fedinabli@gmail.com>
2025-11-15 22:35:33 +0100

    Added coding styles and formatting

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,14 @@
 #include <iostream>
 
-int main()
+#include <cmath>
+#include <cstdint>
+#include <gsl/gsl>
+
+int main(std::int32_t argc, gsl::zstring* argv)
 {
+  gsl::span<gsl::zstring> arguments(argv, argc);
+  std::sort(arguments.begin(), arguments.end());
+
   std::cout << "Hello World!" << std::endl;
   return 0;
 }
